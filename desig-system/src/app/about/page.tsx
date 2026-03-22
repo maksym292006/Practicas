25cd83d73a1024a8923faf5e82cffbaf693641d3
Maksym Hrynenko <mhrynenko@iesmordefuentes.com>
2026-03-22 19:07:15 +0200

    un par de cambios de diseño +uso de componente tag en about/page.tsx

--- desig-system/src/app/about/page.tsx
+++ desig-system/src/app/about/page.tsx
@@ -2,16 +2,17 @@
 import React from "react";
 import { Rocket, Pen, Server } from "lucide-react";
 import BotonLink from "@/components/ui/buttonLink";
+import Tag from "@/components/tags";
 
 export default function AboutPage() {
   return (
     //  Contenedor principal
     <section className="flex flex-col h-full py-20 px-6 max-w-4xl mx-auto">
       {/* Cabecera,tipo boton  */}
-      <p className="border rounded-full px-4 py-1 w-fit mb-4 text-sm">
-        About us
-      </p>
-
+      <div className="mb-10 mx-auto">
+        <Tag context="ABOUT US"></Tag>
+      </div>
+    
       <h1 className="text-3xl font-bold mb-4">We are GoatyDesigners</h1>
       <p className="text-sm text-gray-500 leading-relaxed mb-16 max-w-xl">
         A small studio based in Somontano focused on building clean, scalable
--- desig-system/src/components/transparentCard.tsx
+++ desig-system/src/components/transparentCard.tsx
@@ -10,7 +10,7 @@ function TCard({
   context: string;
 }) {
   return (
-    <div className="flex flex-col items-center text-center max-w-xs">
+    <div className="flex flex-col items-start text-center max-w-xs">
       <span className="text-4xl font-bold mb-4">{nStep}</span>
       <h3 className="text-lg font-semibold mb-2">{title}</h3>
       <p className="text-sm text-gray-500">{context}</p>
