3bcd8d17e9442269401b7291e29af68cc6af8aab
maksym <mhrynenko@iesmordefuentes.com>
2026-03-24 09:58:44 +0100

    barra navegacion transparente

--- desig-system/src/components/ui/navbar.tsx
+++ desig-system/src/components/ui/navbar.tsx
@@ -3,9 +3,10 @@
 export function Navbar({ children }: { children: React.ReactNode }) {
   // Una vez  obtenido el link , lo metemos dentro del nav que aplica tailwind
   return (
-    <nav className="flex gap-4 bg-black p-4 text-white font-thin">
-      {children}
-    </nav>
+    <div className="mx-auto sticky top-0 z-50">
+      <nav className="flex gap-4 justify-center w-2xl bg-ring/80 backdrop-blur-[2px]  border border-black/20 text-black m font-thin rounded-[10] p-2 m-2">
+        {children}
+      </nav>
+    </div>
   );
 }
-
